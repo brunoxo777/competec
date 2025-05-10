181fc38bff8db43f346b9be13e19021f32bfdf0e
brunoxo777 <bruno.sl787@gmail.com>
2025-05-10 11:42:56 -0300

    Update Main.java1

--- aula-1/Main.java
+++ aula-1/Main.java
@@ -1,7 +1,31 @@
 public class Main
 {
+	
+import java.util.Scanner;
+public class Main
+{
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+boolean sair, chovendo, relampejando;
+chovendo = true;
+relampejando = false;
+ if( !(chovendo && relampejando) ) {System.out.println("não sair");}
+ 
+ else if (chovendo) {System.out.println("Sair");}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
 	}
 }
